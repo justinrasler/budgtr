4be0be150b8b48f60c54d80e2c9a39c8d7993d06
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-18 15:43:52 -0400

    budgtr all showing

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express()
+const budget = require("./models/budget")
 
 
 app.use(express.static('public'))
 
 app.get("/budget", (req,res) => {
-    res.send("Hello World")
+    res.render('index.ejs')
+})
+app.get("/budget/:indexOfBudgetArray", (req,res) => {
+    res.send(budget[req.params.indexOfBudgetArray])
 })
 
 app.listen(3000, () => {
     console.log("listening on port 3000")
 })
-

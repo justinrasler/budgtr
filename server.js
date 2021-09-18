53408e3e19fe550078df6e6683d96498d6afa701
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-18 14:54:13 -0400

    first get reposnse working

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express()
+
+
+app.use(express.static('public'))
+
+app.get("/budget", (req,res) => {
+    res.send("Hello World")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
+
